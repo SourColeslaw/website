30fb57fb6279c185af5711bfca8de5a0867495d2
SourColeslaw <colson.foster.sc@gmail.com>
2022-01-24 20:56:08 -0500

    First commit.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function IndexPage() {
   return (
-    <h1>Hello World!</h1>
+    <h1>Caleb, you sussy baka</h1>
   )
 }
+
+<img src="https://i1.sndcdn.com/artworks-Uii8SMJvNPxy8ePA-romBoQ-t500x500.jpg" />
 
 export default IndexPage
