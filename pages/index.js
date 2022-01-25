d67f3cf4517913414dfae1833f7ef4fdab936608
SourColeslaw <colson.foster.sc@gmail.com>
2022-01-24 20:58:59 -0500

    First commit.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 function IndexPage() {
   return (
-    <h1>Caleb, you sussy baka</h1>
+    <h1>WWW.AMONG-US-SUS.COM</h1>
   )
 }
-
-<img src="https://i1.sndcdn.com/artworks-Uii8SMJvNPxy8ePA-romBoQ-t500x500.jpg" />
 
 export default IndexPage
