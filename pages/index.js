a4aae02df3a17379d84db1db3fdf7cb7c123bb4f
SourColeslaw <colson.foster.sc@gmail.com>
2022-04-11 23:24:20 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 function IndexPage() {
   return (
-    <h1>This is Kai</h1>
+    <h1>Test</h1>
   )
 }
-
-var qrKai = document.createElement("img");
-qrKai.src = "Kai.jpg";
-document.body.appendChild(qrKai);
 
 export default IndexPage
