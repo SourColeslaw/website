d2a42fd5c5c8b8c90fab9f2084f167e5c0738718
Colson Foster <colson.foster.sc@gmail.com>
2022-06-26 21:18:18 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function IndexPage() {
       <br>
       </br>
       <img height="500px" width="750px" src="https://cdn.discordapp.com/attachments/970184543342575640/990783798700568577/Morbius_film_poster.jpg.webp"></img>
-      <button type="button">It's morbin' time!</button>
+      <button type="button">Its morbin time!</button>
     </div>
   )
 }
