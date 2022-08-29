aba90797bd5db83ee17f141ef45f1787b9321c47
Colson Foster <colson.foster.sc@gmail.com>
2022-08-28 20:17:14 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function IndexPage() {
       <h1>This is a test for my computer science class</h1>
       <br>
       </br>
-      <img height="500px" width="750px" src="https://cdn.discordapp.com/attachments/970184543342575640/990783798700568577/Morbius_film_poster.jpg.webp"></img>
+      <img height="500px" width="750px" src="https://cdn.discordapp.com/attachments/970184543342575640/1013602596637921330/unknown.png"></img>
       <br>
       </br>
       <button type="button">Test</button>
