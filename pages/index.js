25b90ecca1d52188b75e7799c79757c6bbbde43a
Colson Foster <colson.foster.sc@gmail.com>
2022-08-28 20:12:37 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 function IndexPage() {
   return (
     <div>
-      <h1>I LOVE MORBIUS!!!!!!!!!!!</h1>
+      <h1>This is a test for my computer science class</h1>
       <br>
       </br>
       <img height="500px" width="750px" src="https://cdn.discordapp.com/attachments/970184543342575640/990783798700568577/Morbius_film_poster.jpg.webp"></img>
       <br>
       </br>
-      <button type="button">Its morbin time!</button>
+      <button type="button">Test</button>
     </div>
   )
 }
