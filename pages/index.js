c3dbf0062c2d33f80bf79a4aceb7b9d58fec53b0
SourColeslaw <colson.foster.sc@gmail.com>
2023-01-26 22:06:00 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 function IndexPage() {
   return (
     <div>
-      <h1>This is a test for my computer science class</h1>
-      <br>
-      </br>
-      <img height="500px" width="750px" src="https://cdn.discordapp.com/attachments/970184543342575640/1013602596637921330/unknown.png"></img>
-      <br>
-      </br>
-      <button type="button">Test</button>
     </div>
   )
 }
